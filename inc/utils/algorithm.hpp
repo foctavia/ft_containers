47c0e65b20ed6dadb2e880ce12c42a475391ca10
Fany Octavia <foctavia@student.42.fr>
2023-02-01 11:43:06 +0100

    Create algo for ft::equal and ft::lex_comp

---
+++ inc/utils/algorithm.hpp
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algorithm.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: foctavia <foctavia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 10:20:16 by foctavia          #+#    #+#             */
+/*   Updated: 2023/02/01 11:41:10 by foctavia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ALGORITHM_HPP
+# define ALGORITHM_HPP
+
+namespace ft
+{
+	/* LEXICOGRAPHICAL_COMPARE ************************************************************ */
+
+	template< class InputIt1, class InputIt2 >
+	bool	lexicographical_compare( InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2 )
+	{
+		for (; (first1 != last1) && (first2 != last2); ++first1, ++first2)
+		{
+			if (*first1 < *first2)
+				return true;
+			if (*first2 < *first1)
+				return false;
+		}
+ 
+    	return (first1 == last1) && (first2 != last2);
+	}
+
+	template< class InputIt1, class InputIt2, class Compare >
+	bool	lexicographical_compare( InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, Compare comp )
+	{
+		for (; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2)
+		{
+			if (comp(*first1, *first2))
+				return true;
+			if (comp(*first2, *first1))
+				return false;
+		}
+	
+		return (first1 == last1) && (first2 != last2);
+	}
+
+	/* EQUAL ****************************************************************************** */
+	
+	template< class InputIt1, class InputIt2 >
+	bool	equal( InputIt1 first1, InputIt1 last1, InputIt2 first2 )
+	{
+		for (; first1 != last1; ++first1, ++first2)
+		{
+			if (!(*first1 == *first2)) 
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	template< class InputIt1, class InputIt2, class BinaryPredicate >
+	bool	equal( InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate p )
+	{
+		    for (; first1 != last1; ++first1, ++first2)
+			{
+				if (!p(*first1, *first2))
+				{
+					return false;
+				}
+			}
+			
+			return true;
+	}
+	
+}
+
+#endif
