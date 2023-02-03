7cda6b2e066ce5314f726e0f3d9154133bd72e91
Fany Octavia <foctavia@student.42.fr>
2023-02-03 15:19:20 +0100

    Corrected ft::stack using ft::vector as container_type

--- inc/containers/stack.hpp
+++ inc/containers/stack.hpp
@@ -6,23 +6,23 @@
 /*   By: foctavia <foctavia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 12:23:43 by foctavia          #+#    #+#             */
-/*   Updated: 2023/01/31 18:27:26 by foctavia         ###   ########.fr       */
+/*   Updated: 2023/02/03 15:18:08 by foctavia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STACK_HPP
 # define STACK_HPP
 
-// to be changed with ft::vector (not allowed to use any std::container library)
-# include <deque>
+# include <vector.hpp>
 
 namespace ft
 {
 	/* STACK ****************************************************************************** */
 	
-	template< class T, class Container = std::deque<T> >
+	template< class T, class Container = ft::vector< T, std::allocator< T > > >
 	class stack
 	{
+		
 		public:
 		
 			typedef Container									container_type;
@@ -32,12 +32,12 @@ namespace ft
 			typedef typename container_type::const_reference	const_reference;
 
 	// CONSTRUCTOR
-			
-			explicit stack( const container_type &cont = container_type() ) : _c( cont ) {}
+			explicit stack( const container_type &cont = container_type() )
+				: _c( cont )								{ }
 	
 	// DESTRUCTOR
 
-			~stack( void ) { this->_c.clear(); }
+			~stack( void )									{ this->_c.clear(); }
 			
 	// ASSIGNMENT OPERATOR
 
@@ -49,64 +49,76 @@ namespace ft
 			}
 
 	// MEMBER FUNCTIONS
+	
 		// Member functions for Element access
 
-			reference		top( void ) { return this->_c.back(); }
-			const_reference	top( void ) { return this->_c.back(); }
+			reference		top( void )						{ return this->_c.back(); }
+			const_reference	top( void ) const				{ return this->_c.back(); }
 
 		// Member functions for Capacity
 		
-			bool			empty( void ) const { return this->_c.empty(); }
-			size_type		size( void ) const { return this->_c.size(); }
+			bool			empty( void ) const				{ return this->_c.empty(); }
+			
+			size_type		size( void ) const				{ return this->_c.size(); }
 
 		// Member functions for Modifiers
 
-			void			push( const value_type &value ) { this->_c.push_back(value); }
-			void			pop( void ) { this->_c.pop_back(); }
+			void			push( const value_type &value )	{ this->_c.push_back(value); }
+			
+			void			pop( void )						{ this->_c.pop_back(); }
 
-	// NON-MEMBER FUNCTIONS
-		
-			template< class T, class Container >
-			bool	operator==( const ft::vector<T, Allocator> &lhs, const ft::vector<T, Allocator> &rhs )
-			{
-				return (lhs._c == rhs._c);
-			}
+	// NON-MEMBER FUNCTION PROTOTYPE (using friend keyword)
+
+			template< class _T, class _Container >
+			friend inline bool	operator==( const stack< _T, _Container > &lhs, const stack< _T, _Container > &rhs );
 			
-			template< class T, class Container >
-			bool	operator!=( const ft::vector<T, Allocator> &lhs, const ft::vector<T, Allocator> &rhs )
-			{
-				return (lhs._c != rhs._c);
-			}
-			
-			template< class T, class Container >
-			bool	operator<( const ft::vector<T, Allocator> &lhs, const ft::vector<T, Allocator> &rhs )
-			{
-				return (lhs._c < rhs._c);
-			}
-			
-			template< class T, class Container >
-			bool	operator<=( const ft::vector<T, Allocator> &lhs, const ft::vector<T, Allocator> &rhs )
-			{
-				return (lhs._c <= rhs._c);
-			}
-			
-			template< class T, class Container >
-			bool	operator>( const ft::vector<T, Allocator> &lhs, const ft::vector<T, Allocator> &rhs )
-			{
-				return (lhs._c > rhs._c);
-			}
-			
-			template< class T, class Container >
-			bool	operator>=( const ft::vector<T, Allocator> &lhs, const ft::vector<T, Allocator> &rhs )
-			{
-				return (lhs._c >= rhs._c);
-			}
-			
+			template< class _T, class _Container >
+			friend inline bool	operator<( const stack< _T, _Container > &lhs, const stack< _T, _Container > &rhs );
+
 		protected:
 
 			container_type	_c;
 			
 	};
+	
+	// NON-MEMBER FUNCTIONS
+		
+		template< class T, class Container >
+		inline bool	operator==( const ft::stack< T, Container > &lhs, const ft::stack< T, Container > &rhs )
+		{
+			return (lhs._c == rhs._c);
+		}
+		
+		template< class T, class Container >
+		inline bool	operator<( const ft::stack< T, Container > &lhs, const ft::stack< T, Container > &rhs )
+		{
+			return (lhs._c < rhs._c);
+		}
+		
+		template< class T, class Container >
+		inline bool	operator!=( const ft::stack< T, Container > &lhs, const ft::stack< T, Container > &rhs )
+		{
+			return !(lhs == rhs);
+		}
+		
+		template< class T, class Container >
+		inline bool	operator>( const ft::stack< T, Container > &lhs, const ft::stack< T, Container > &rhs )
+		{
+			return (rhs < lhs);
+		}
+		
+		template< class T, class Container >
+		inline bool	operator<=( const ft::stack< T, Container > &lhs, const ft::stack< T, Container > &rhs )
+		{
+			return !(rhs < lhs);
+		}
+		
+		template< class T, class Container >
+		inline bool	operator>=( const ft::stack< T, Container > &lhs, const ft::stack< T, Container > &rhs )
+		{
+			return !(lhs < rhs);
+		}
+			
 }
 
 #endif
